3de02117f7b70158914a784c0a5806fc8d2c3d86
Francesco Pierfederici <me@pythoninside.com>
2020-08-17 19:30:02 +0200

    publishing to mqtt server

--- tilt.py
+++ tilt.py
@@ -1,7 +1,9 @@
 from machine import I2C, Pin, RTC, DEEPSLEEP, deepsleep
 import math
+import network
 import os
 import time
+from mqtt.robust import MQTTClient
 import ujson
 import mpu6050
 
@@ -16,6 +18,7 @@ CONFIG_FILE = 'config.json'
 
 
 def parse_config_file(config_file, defaults=DEFAULTS):
+    # TODO: recursive dict update
     config = dict(defaults)
 
     try:
@@ -29,11 +32,25 @@ def parse_config_file(config_file, defaults=DEFAULTS):
     return config
 
 
+def setup_wifi(ssid, password):
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
+
+    if not sta_if.isconnected():
+        sta_if.active(True)
+        sta_if.connect(ssid, password)
+        while not sta_if.isconnected():
+            time.sleep_ms(50)
+
+
 config = parse_config_file(CONFIG_FILE)
 i2c = I2C(scl=Pin(config['scl_pin']),
           sda=Pin(config['sda_pin']),
           freq=400000)
 mpu6050.MPU_Init(i2c, int(config['mpu6050_addr']))
+setup_wifi(config['wifi']['ssid'], password=config['wifi']['password'])
+client = MQTTClient(config['mqtt']['client_id'], config['mqtt']['broker'])
 
 tilts = []
 for _ in range(config['num_samples']):
@@ -51,6 +68,10 @@ if config['num_samples'] % 2 == 0:
 else:
     tilt = tilts[config['num_samples'] // 2]
 
+client.connect()
+client.publish(config['mqtt']['topic'] + '/tilt', b'%.02f' % (tilt))
+client.publish(config['mqtt']['topic'] + '/temperature', b'%.02f' % (temp))
+client.disconnect()
 print('Tilt: %.2f  Temp: %.2f C' % (tilt, temp))
 
 rtc = RTC()
